506f8a968436e4dae3b4824ba0503d1ad98d1349
eyob tariku <eyobtariku@gmail.com>
2017-08-10 09:25:26 +0300

    Fix minor bug

--- core/settings.py
+++ core/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['./templates'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
